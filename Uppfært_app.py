5f0fe375bb70ec6e6225f1e5934001f36aba1770
Jonschth <jst@arev.is>
2025-03-30 17:20:12 +0000

    Updated program

--- Uppfært_app.py
+++ Uppfært_app.py
@@ -794,6 +794,8 @@ def server(input, output, session):
 
        df_beta_krókur = df_beta_krókur.loc[(df_beta_krókur != 0).any(axis=1)]
 
+       df_beta_krókur = df_beta_krókur.mul(100).round(2)
+
        return df_beta_krókur.mul(100).reset_index().rename(columns={'index': 'Fyrirtæki'})
 
     @render.table
